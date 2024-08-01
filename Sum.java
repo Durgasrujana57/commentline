b13a2ce1ce15993290827a62fb907dda1c5ead37
Durgasrujana57 <durgasrujana570@gmail.com>
2024-08-01 16:27:36 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,10 @@
+class Sum {
+  public static void main(String args[]) {
+  int sum=0;
+  int A[]={4,5,6,7,8};
+  for(int i=0;i<A.length;i++)
+    sum+=A[i];
+    System.out.println(sum);
+    }
+   } 
+  
